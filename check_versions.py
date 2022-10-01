dd61c0e32d50b3d6f395a158c985b9cf16d4919d
OpsDevIT <info@opsdevit.nl>
2022-10-01 16:38:28 +0200

    iteration 2: auto update

--- check_versions.py
+++ check_versions.py
@@ -23,19 +23,42 @@ if __name__ == "__main__":
   packages = {}
   packages[kind] = (get_packages(kind))
 
-
-  subprocess.run("apk update", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+  if kind == "apk":
+    subprocess.run("apk update", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+  
   for package in packages[kind]:
     package_name = package.split()[1]
+    # print(package_name)
 
     package_current = package.split()[2]
+    
     if kind == "apk":
       package_latest = subprocess.run("apk -q info " + package_name + " | head -n1 | awk '{print $1}' | sed -r 's/^" + package_name + "-(.*)/\\1/g'", shell=True, capture_output=True, text=True).stdout.strip()
-    
+
+    if kind == "container":
+      if "alpine" in package_name:
+        package_latest = subprocess.run("skopeo list-tags docker://" + package_name + " | jq -r \".Tags[]\" | sort -Vr | grep -v '^[a-z]' | grep \"\\.\" | head -n 1", shell=True, capture_output=True, text=True).stdout.strip()
+      else:
+        print("add config for new package")
+
+    if kind == "pip":
+      package_latest = subprocess.run("yolk -M" + package_name + " -f version", shell=True, capture_output=True, text=True).stdout.strip()
+
+    if kind == "github":
+      if "kustomize" in package_name:
+        # print("echoo")
+        package_latest = subprocess.run("curl -s https://api.github.com/repos/" + package_name + " | jq -r .[].tag_name | grep kustomize | sort -Vr | sed -r 's/^kustomize\\/(.*)/\\1/g' | head -n 1", shell=True, capture_output=True, text=True).stdout.strip()
+      if "sealed-secrets" in package_name:
+        package_latest = subprocess.run("curl -s https://api.github.com/repos/" + package_name + " | jq -r .[].tag_name | grep ^v | sort -rV | head -n 1", shell=True, capture_output=True, text=True).stdout.strip()
+      else:
+        print("add config for new package")
+
     print(package_current)
     print(package_latest)
     if package_current == package_latest:
       print("matched\n")
+    else:
+      print("upgrade me\n")
     
     ### TODO
     ### als geen match dan latest pushen naar nieuwe version file, container builden en tests draaien
